20acc1a72047edc9895bd2dd9689ca8dc8719400
meow6361 <130595112+meow6361@users.noreply.github.com>
2023-04-13 20:49:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from "vite";
 export default defineConfig({
-  server:{
-    host:true,
-    port:80,
-    
-  }
-})
+  server: {
+    host: true,
+    port: 80,
+   cors: true
+  },
+});
